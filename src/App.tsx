dc8ce97a2ffd0d421a13ed6d550fd47b365659ce
klashz <amirgataullin04@gmail.com>
2024-05-20 14:16:46 +0300

    empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './styles/App.css';
-import FormToPdf from './components/FormToPdf';
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>PDF Generator</h1>
-      <FormToPdf />
+      <h1>Title</h1>
     </div>
   );
 };
